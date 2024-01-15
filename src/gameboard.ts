f7841f24d9acbc44b9852f0456abaf16d6f37c40
jolley34 <erlandssonjoel@gmail.com>
2024-01-15 17:25:56 +0100

    Co-authored-by: Maryamakeyf <Maryamakeyf@users.noreply.github.com> Co-authored-by: CatAvadani <CatAvadani@users.noreply.github.com> Co-authored-by: Annamedie <Annamedie@users.noreply.github.com> Co-authored-by: AliAlahmad1981 <AliAlahmad1981@users.noreply.github.com> Co-authored-by: Malin Sundberg <Malinsund@users.noreply.github.com>

---
+++ src/gameboard.ts
@@ -0,0 +1,58 @@
+let backgroundImage: p5.Image;
+
+class GameBackground {
+  backgroundImage: p5.Image;
+
+  constructor() {
+    this.backgroundImage = loadImage("./assets/background/map1-blurred.png");
+  }
+
+  public setupGameBackground() {
+    createCanvas(windowWidth, windowHeight);
+  }
+
+  public drawGameBackground() {
+    background(this.backgroundImage);
+  }
+}
+
+class Gameboard {
+  positionX: number;
+  positionY: number;
+
+  private player1: Player;
+
+  constructor() {
+    //default value till position
+    this.positionX = 0;
+    this.positionY = 0;
+
+    this.player1 = new Player("black", 100, 100, {
+      up: 87, // w
+      left: 65, // a
+      down: 83, // s
+      right: 68, // d
+    });
+  }
+  public update() {
+    this.player1.update();
+  }
+
+  public setupGameboard() {}
+
+  public preloadGameboard() {}
+
+  public drawGameboard() {
+    createCanvas(1000, 1000);
+    background("red");
+    push();
+    this.player1.drawPlayer();
+    this.update(); 
+
+  }
+
+}
+
+
+
+
