8fb813b892d98335f36a7b6b04993eef8b7ff4f4
jolley34 <erlandssonjoel@gmail.com>
2024-01-18 15:47:11 +0100

    gitgit

--- src/gameEntity.ts
+++ src/gameEntity.ts
@@ -9,9 +9,5 @@ class GameEntity {
 
   public update(): void {}
 
-  // Declare a draw method that can be overridden by subclasses
-  public draw(): void {
-    // You can leave it empty here or provide a default behavior
-    // or alternatively, mark it as abstract if it should be implemented in subclasses.
-  }
+  public draw(): void {}
 }
