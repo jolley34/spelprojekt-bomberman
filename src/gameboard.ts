ab48eca524116a95de88530f5d52c673bf3ca81b
jolley34 <erlandssonjoel@gmail.com>
2024-01-18 14:42:33 +0100

    molnen går över igen

--- src/gameboard.ts
+++ src/gameboard.ts
@@ -11,11 +11,12 @@ class GameBoard {
   // Define blockSize
   public draw() {
     background(assets.images.background);
-    this.clouds.draw();
 
     // Loop over all entities and draw them
     for (let i = 0; i < this.entities.length; i++) {
       this.entities[i].draw();
     }
+
+    this.clouds.draw();
   }
 }
