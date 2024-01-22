327d470002a574d9e35f538bbced836c24248061
jolley34 <erlandssonjoel@gmail.com>
2024-01-22 19:24:02 +0100

    uppdaterade moln så att size och opacity kan ändras separat på varje moln

--- src/clouds.ts
+++ src/clouds.ts
@@ -4,6 +4,8 @@ class Clouds {
     x: number;
     y: number;
     speed: number;
+    size: number;
+    opacity: number;
   }[] = [];
 
   constructor() {
@@ -14,72 +16,96 @@ class Clouds {
         x: 400,
         y: 550,
         speed: 0.25,
+        size: 300,
+        opacity: 180,
       },
       {
         image: assets.images.clouds[0],
         x: 1000,
         y: 300,
         speed: 0.25,
+        size: 300,
+        opacity: 180,
       },
       {
         image: assets.images.clouds[0],
         x: -50,
         y: 5,
         speed: 0.25,
+        size: 300,
+        opacity: 180,
       },
       {
         image: assets.images.clouds[0],
         x: -200,
         y: 200,
         speed: 0.3,
+        size: 300,
+        opacity: 180,
       },
       {
         image: assets.images.clouds[0],
         x: -200,
         y: 400,
         speed: 0.2,
+        size: 300,
+        opacity: 180,
       },
       {
         image: assets.images.clouds[0],
         x: -200,
         y: 600,
         speed: 0.3,
+        size: 300,
+        opacity: 180,
       },
       {
         image: assets.images.clouds[0],
         x: 0,
         y: 50,
         speed: 0.22,
+        size: 300,
+        opacity: 180,
       },
       {
         image: assets.images.clouds[0],
         x: -1000,
         y: 5,
         speed: 0.25,
+        size: 300,
+        opacity: 180,
       },
       {
         image: assets.images.clouds[0],
         x: -1100,
         y: 200,
         speed: 0.3,
+        size: 300,
+        opacity: 180,
       },
       {
         image: assets.images.clouds[0],
         x: -1200,
         y: 400,
         speed: 0.2,
+        size: 300,
+        opacity: 180,
       },
       {
         image: assets.images.clouds[0],
         x: -1500,
         y: 600,
         speed: 0.3,
+        size: 300,
+        opacity: 180,
       },
       {
         image: assets.images.clouds[0],
         x: -900,
         y: 50,
         speed: 0.22,
+        size: 300,
+        opacity: 180,
       },
     ];
   }
@@ -88,18 +114,14 @@ class Clouds {
     for (let i = 0; i < this.clouds.length; i++) {
       let cloud = this.clouds[i];
 
-      let opacity = 180;
-
-      tint(255, opacity);
-
-      image(cloud.image, cloud.x, cloud.y, 300, 300);
-
+      tint(255, cloud.opacity);
+      image(cloud.image, cloud.x, cloud.y, cloud.size, cloud.size);
       noTint();
 
       cloud.x += cloud.speed;
 
       if (cloud.x > width) {
-        cloud.x = -100;
+        cloud.x = -cloud.size;
       }
     }
   }
