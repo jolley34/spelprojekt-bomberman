68ded7bc41e5fef4b2a12a09c8fd0ba4836b6f34
jolley34 <erlandssonjoel@gmail.com>
2024-01-22 16:32:03 +0100

    musik volymen på hälften ingame

--- src/chooseBoard.ts
+++ src/chooseBoard.ts
@@ -37,6 +37,7 @@ class ChooseBoard {
   private handleGardenBoardSelection() {
     const gardenBoardNumber = this.chooseGardenBoard();
     this.game.changePage("GameBoard", gardenBoardNumber);
+    assets.music.ingamemusic.setVolume(0.5);
     assets.music.ingamemusic.play();
     assets.music.menumusic.stop();
   }
