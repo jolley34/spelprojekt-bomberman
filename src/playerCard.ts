1f655b4de2adfe6924f27550837e3d3acc645718
CatAvadani <catalinaavadani@gmail.com>
2024-01-23 13:58:20 +0100

    Drawing player's hearts / lives.

--- src/playerCard.ts
+++ src/playerCard.ts
@@ -22,6 +22,18 @@ class PlayerCard {
   public removeLife() {
     this.lives--;
   }
+
+  private drawHeart(x: number, y: number) {
+    push();
+    fill(255, 0, 0);
+    beginShape();
+    vertex(x, y);
+    bezierVertex(x - 15, y - 15, x - 30, y + 20, x, y + 30);
+    bezierVertex(x + 30, y + 20, x + 15, y - 15, x, y);
+    endShape(CLOSE);
+    pop();
+  }
+
   public draw() {
     push();
     fill(0);
@@ -34,6 +46,12 @@ class PlayerCard {
     textAlign(CENTER, CENTER);
     text(this.name, this.positionX, this.positionY);
     pop();
+
+    const heartSpacing = 50;
+    const startX = this.positionX - (heartSpacing * (this.lives - 1)) / 2;
+    for (let i = 0; i < this.lives; i++) {
+      this.drawHeart(startX + i * heartSpacing, this.positionY + 50);
+    }
   }
 
   public addPowerUp() {}
