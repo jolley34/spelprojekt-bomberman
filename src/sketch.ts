f975f23cfe66297c7c506a8eb0407175f0144b22
ipieropan <igorpieropan@gmail.com>
2024-01-23 10:36:52 +0100

    added menu music

--- src/sketch.ts
+++ src/sketch.ts
@@ -1,18 +1,17 @@
 //---- GLOBAL VARIABLES ----//
 let game: Game;
 let assets: {
-  /*   music: {
-    ingamemusic: p5.SoundFile;
-    menumusic: p5.SoundFile;
-  }; */
-  images: {
-    maptextures: p5.Image[];
-    backgroundImages: p5.Image[];
-    clouds: p5.Image[];
-    entities: p5.Image[];
-  };
+	music: {
+		backgroundMusicMenu: p5.SoundFile;
+		backgroundMusicInGame: p5.SoundFile;
+	};
+	images: {
+		maptextures: p5.Image[];
+		backgroundImages: p5.Image[];
+		clouds: p5.Image[];
+		entities: p5.Image[];
+	};
 };
-
 let customFont: p5.Font;
 
 /**
@@ -21,43 +20,44 @@ let customFont: p5.Font;
  * sound files, images etc...
  */
 function preload() {
-  customFont = loadFont("../Fonts/MinecraftBold-nMK1.otf");
-  assets = {
-    /*   music: {
-      ingamemusic: loadSound("/assets/music/spelprojekt-ingame-v6.mp3"),
-      menumusic: loadSound("/assets/music/spelprojekt-menu.mp3"),
-    }, */
-    images: {
-      backgroundImages: [
-        loadImage("../assets/background/Controls.svg"),
-        loadImage("../assets/background/Map-1-blurred-shadow-v4.png"),
-        loadImage("../assets/background/winter_background.png"),
-        loadImage("../assets/background/cruel_nature_bg1.png"),
-      ] /*
+	customFont = loadFont("../Fonts/MinecraftBold-nMK1.otf");
+	assets = {
+		music: {
+			backgroundMusicMenu: loadSound("../assests/music/spelprojekt-menu.mp3"),
+			backgroundMusicInGame: 
+				loadSound("../assests/music/spelprojekt-ingame-v6.mp3"),
+		},
+		images: {
+			backgroundImages: [
+				loadImage("../assets/background/Controls.svg"),
+				loadImage("../assets/background/Map-1-blurred-shadow-v4.png"),
+				loadImage("../assets/background/winter_background.png"),
+				loadImage("../assets/background/cruel_nature_bg1.png"),
+			] /*
     music: {
       ingamemusic: loadSound("/assets/music/spelprojekt-ingame-v6.mp3"),
       menumusic: loadSound("/assets/music/spelprojekt-menu.mp3"),
     }, */,
-      clouds: [
-        loadImage("./assets/clouds/smoke1.png"),
-        loadImage("./assets/clouds/smoke2.png"),
-        loadImage("./assets/clouds/smoke3.png"),
-        loadImage("./assets/clouds/smoke4.png"),
-      ],
-      maptextures: [
-        loadImage("../assets/maptextures/bush.png"),
-        loadImage("../assets/maptextures/ice1.png"),
-        loadImage("../assets/maptextures/brick.png"),
-        loadImage("../assets/maptextures/grass.jpeg"),
-        loadImage("../assets/maptextures/grassywall.png"),
-      ],
-      entities: [
-        loadImage("../assets/entities_img/image1.png"),
-        loadImage("../assets/entities_img/image2.png"),
-        loadImage("../assets/entities_img/image3.png"),
-      ],
-    },
-  };
+			clouds: [
+				loadImage("./assets/clouds/smoke1.png"),
+				loadImage("./assets/clouds/smoke2.png"),
+				loadImage("./assets/clouds/smoke3.png"),
+				loadImage("./assets/clouds/smoke4.png"),
+			],
+			maptextures: [
+				loadImage("../assets/maptextures/bush.png"),
+				loadImage("../assets/maptextures/ice1.png"),
+				loadImage("../assets/maptextures/brick.png"),
+				loadImage("../assets/maptextures/grass.jpeg"),
+				loadImage("../assets/maptextures/grassywall.png"),
+			],
+			entities: [
+				loadImage("../assets/entities_img/image1.png"),
+				loadImage("../assets/entities_img/image2.png"),
+				loadImage("../assets/entities_img/image3.png"),
+			],
+		},
+	};
 }
 
 /**
@@ -67,10 +67,10 @@ function preload() {
  * in the draw function belows
  */
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  textFont(customFont);
-  frameRate(120);
-  game = new Game();
+	createCanvas(windowWidth, windowHeight);
+	textFont(customFont);
+	frameRate(120);
+	game = new Game();
 }
 
 /**
@@ -79,13 +79,20 @@ function setup() {
  * you created in the setup function above
  */
 function draw() {
-  game.update();
-  game.draw();
+	game.update();
+	game.draw();
 }
 
 /**
  *  Built in windowResize listener function in P5
  */
 function windowResized() {
-  /*   resizeCanvas(windowWidth, windowHeight); */
+	/*   resizeCanvas(windowWidth, windowHeight); */
+}
+
+function backgroundMusicMenu(){
+  assets.music.backgroundMusicMenu.play();
+  assets.music.backgroundMusicMenu.loop();
+  assets.music.backgroundMusicMenu.setVolume(0, 3);
+  userStartAudio();
 }
