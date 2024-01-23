57b9efdb457cb521aedd8f991a920001408d1f58
jolley34 <erlandssonjoel@gmail.com>
2024-01-23 01:39:29 +0100

    updated cloyds code , so its randomise aswell as the flowers

--- src/clouds.ts
+++ src/clouds.ts
@@ -6,123 +6,32 @@ class Clouds {
     speed: number;
     size: number;
     opacity: number;
-  }[] = [];
+  }[];
 
   constructor() {
-    //Våg 1 av mo,
-    this.clouds = [
-      {
-        image: assets.images.clouds[0],
-        x: 400,
-        y: 550,
-        speed: 0.25,
-        size: 300,
-        opacity: 160,
-      },
-      {
-        image: assets.images.clouds[0],
-        x: 1000,
-        y: 300,
-        speed: 0.25,
-        size: 300,
-        opacity: 160,
-      },
-      {
-        image: assets.images.clouds[0],
-        x: -50,
-        y: 5,
-        speed: 0.25,
-        size: 300,
-        opacity: 160,
-      },
-      {
-        image: assets.images.clouds[0],
-        x: -200,
-        y: 200,
-        speed: 0.3,
-        size: 300,
-        opacity: 160,
-      },
-      {
-        image: assets.images.clouds[0],
-        x: -200,
-        y: 400,
-        speed: 0.2,
-        size: 300,
-        opacity: 160,
-      },
-      {
-        image: assets.images.clouds[0],
-        x: -200,
-        y: 600,
-        speed: 0.3,
-        size: 300,
-        opacity: 160,
-      },
-      {
-        image: assets.images.clouds[0],
-        x: 0,
-        y: 50,
-        speed: 0.22,
-        size: 300,
-        opacity: 160,
-      },
-      {
-        image: assets.images.clouds[0],
-        x: -1000,
-        y: 5,
-        speed: 0.25,
-        size: 300,
-        opacity: 160,
-      },
-      {
-        image: assets.images.clouds[0],
-        x: -1100,
-        y: 200,
-        speed: 0.3,
-        size: 300,
-        opacity: 160,
-      },
-      {
-        image: assets.images.clouds[0],
-        x: -1200,
-        y: 400,
-        speed: 0.2,
-        size: 300,
-        opacity: 160,
-      },
-      {
-        image: assets.images.clouds[0],
-        x: -1500,
-        y: 600,
-        speed: 0.3,
-        size: 300,
-        opacity: 160,
-      },
-      {
-        image: assets.images.clouds[0],
-        x: -900,
-        y: 50,
-        speed: 0.22,
-        size: 300,
-        opacity: 160,
-      },
-    ];
+    this.clouds = [];
+    for (let i = 0; i < 13; i++) {
+      this.clouds.push({
+        image: random(assets.images.clouds),
+        y: random(height),
+        x: random(width),
+        speed: random(0.15, 0.4),
+        size: random(125, 250),
+        opacity: random(100, 140),
+      });
+    }
   }
 
-  public draw() {
+  draw() {
     for (let i = 0; i < this.clouds.length; i++) {
-      let cloud = this.clouds[i];
-
-      tint(255, cloud.opacity);
-      image(cloud.image, cloud.x, cloud.y, cloud.size, cloud.size);
-      noTint();
-
-      cloud.x += cloud.speed;
-
-      if (cloud.x > width) {
-        cloud.x = -cloud.size;
+      const clouds = this.clouds[i];
+      clouds.x += clouds.speed;
+      if (clouds.x > width) {
+        clouds.x = -clouds.size;
       }
+      tint(255, clouds.opacity);
+      image(clouds.image, clouds.x, clouds.y, clouds.size, clouds.size);
+      noTint();
     }
   }
 }
