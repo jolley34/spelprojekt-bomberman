9dd00140aa1368a178eddc0c6ec905588e9d688a
malin <malleballe90@gmail.com>
2024-01-23 14:16:32 +0100

    made the power-up temporary

--- src/player.ts
+++ src/player.ts
@@ -8,6 +8,8 @@ class Player extends GameEntity {
   private upAnimationLoop: number[];
   private downAnimationLoop: number[];
   private increasedSpeed: number;
+  private powerUpDuration: number;
+  private powerUpTimer: number;
 
   constructor(x: number, y: number, size: number) {
     super(assets.images.player1Animations[0], x, y, size);
@@ -16,6 +18,8 @@ class Player extends GameEntity {
     this.animationIndex = 0;
     this.animationSpeed = 0.8;
     this.increasedSpeed = 2;
+    this.powerUpDuration = 10000;
+    this.powerUpTimer = 0;
 
     // Vilka bilder jag loopar igenom när jag trycker vänster
     this.leftAnimationLoop = [7, 6, 8, 6];
@@ -42,10 +46,17 @@ class Player extends GameEntity {
       this.speedX = 0;
       this.speedY = 0;
     }
-
     // Ändra position utifrån hastighet
     this.x += this.speedX;
     this.y += this.speedY;
+
+    //kollar om power up är tagen och om tiden runnit ut
+    if (this.powerUpTimer > 0){
+      this.powerUpTimer -= deltaTime;
+      if (this.powerUpTimer <= 0){
+        this.resetPowerUp();
+      }
+    }
   }
 
   private animateLeft(): void {
@@ -102,11 +113,17 @@ class Player extends GameEntity {
 
   // powerup "increaseSpeed" gör att spelaren ökar farten efter den har plockat upp powerup, "getEffectiveSpeed" ligger under kontrollerna högre upp i koden
   private getEffectiveSpeed(): number {
-    return this.increasedSpeed > 0 ? this.increasedSpeed : 4;
+    return this.increasedSpeed > 0 && this.powerUpTimer > 0 ? this.increasedSpeed : 4;
   }
 
   // hur mycket farten skall öka för spelaren efter powerup
   public increaseSpeed(): void {
     this.increasedSpeed = 6;
+    this.powerUpTimer = this.powerUpDuration;
+  }
+
+  private resetPowerUp(): void {
+    this.increasedSpeed = 0;
+    this.powerUpTimer = 0;
   }
 }
