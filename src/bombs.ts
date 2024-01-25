0942b25b52a4bc287aed33f6abd2cfecc68a5a1e
annaa <annaalehed@hotmail.com>
2024-01-25 11:46:09 +0100

    en början på timer

--- src/bombs.ts
+++ src/bombs.ts
@@ -2,13 +2,24 @@
 class Bomb extends GameEntity {
   private timer: number;
   private range: number;
+  private hasExploaded: boolean;
 
   constructor(x: number, y: number, size: number) {
     super(assets.images.bombs[0], x, y, size);
-    this.timer = 3000;
+    this.timer = 300;
     this.range = 50;
+    this.hasExploaded = true;
+  }
+  public update(): void {
+    this.explosionDelay();
   }
   private explosionDelay(): void {
-    const startTime = this.timer;
+    if (!this.hasExploaded) {
+      this.timer--;
+    }
+    if (this.timer <= 0) {
+      this.explosion(this.hasExploaded);
+    }
   }
+  private explosion(hasExploaded: boolean) {}
 }
