cf4fedbcc073add8727a87ada3832c906e5645cd
annaa <annaalehed@hotmail.com>
2024-01-25 19:28:18 +0100

    en utsmetad bomb!

--- src/bombs.ts
+++ src/bombs.ts
@@ -12,22 +12,37 @@ class Bomb extends GameEntity {
     this.range = 50;
     this.hasExploaded = false;
   }
-  public update(): void {
-    this.explosionDelay();
+  public update(gameBoard: IAddEntity): void {
+    this.explosionDelay(gameBoard);
   }
-  private explosionDelay(): void {
+  private explosionDelay(gameBoard: IAddEntity): void {
     this.bombTimer--;
 
     if (this.bombTimer <= 0) {
-      this.explosion();
+      this.explosion(gameBoard);
       this.hasExploaded = true;
     }
   }
-  private explosion() {
+  private explosion(gameBoard: IAddEntity) {
     if (this.hasExploaded) {
       console.log("jag är här");
       this.image = assets.images.bombs[3];
-      this.explosionTimer--;
+
+      for (let xOffset = -this.range; xOffset <= this.range; xOffset += 25) {
+        for (let yOffset = -this.range; yOffset <= this.range; yOffset += 25) {
+          // Create a new GameEntity with the explosion image
+          const explosionImage = assets.images.bombs[3];
+          gameBoard.addEntity(
+            new GameEntity(
+              explosionImage,
+              this.x + xOffset,
+              this.y + yOffset,
+              25
+            )
+          );
+        }
+        this.explosionTimer--;
+      }
     }
   }
 }
