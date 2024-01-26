7eea3c1eb81514888c589826a6c656b2d495c23d
CatAvadani <catalinaavadani@gmail.com>
2024-01-26 17:28:52 +0100

    Update font and color.

--- src/endOfGame.ts
+++ src/endOfGame.ts
@@ -43,11 +43,11 @@ class EndOfGame {
     rectMode(CENTER);
     rect(width / 2, height / 2 - 50, 500, 200, 10);
 
-    fill(255);
-    textSize(32);
+    fill("#B3D917");
+    textSize(50);
     textAlign(CENTER, CENTER);
     text("WINNER", width / 2, height / 2 - 100);
-    textSize(24);
+    textSize(40);
     text(this.displayWinner, width / 2, height / 2 - 60);
     text(`SCORE: ${this.displayScore}`, width / 2, height / 2 - 20);
     pop();
