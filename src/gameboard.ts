00d177e3479be5835dc0497b7e036b54017fcf99
ipieropan <igorpieropan@gmail.com>
2024-01-30 13:40:54 +0100

    hitbox working

--- src/gameboard.ts
+++ src/gameboard.ts
@@ -1,291 +1,287 @@
 interface IAddEntity {
-  addEntity(entity: GameEntity): void;
-  entities: GameEntity[];
+	addEntity(entity: GameEntity): void;
+	entities: GameEntity[];
 }
 
 class GameBoard implements IAddEntity {
-  private clouds: Clouds;
-  private flowers: Flowers;
-  public entities: GameEntity[];
-  private backgroundImage: p5.Image;
-  private timer: Timer;
-  private playerCard1: PlayerCard;
-  private playerCard2: PlayerCard;
-  private endOfGame: EndOfGame;
-  private icon: p5.Image;
+	private clouds: Clouds;
+	private flowers: Flowers;
+	public entities: GameEntity[];
+	private backgroundImage: p5.Image;
+	private timer: Timer;
+	private playerCard1: PlayerCard;
+	private playerCard2: PlayerCard;
+	private endOfGame: EndOfGame;
+	private icon: p5.Image;
 
-  private isGameActive: boolean;
+	private isGameActive: boolean;
 
-  constructor(
-    entities: GameEntity[],
-    backgroundImage: p5.Image,
-    icon: p5.Image
-  ) {
-    this.clouds = new Clouds();
-    this.flowers = new Flowers();
-    this.entities = entities;
-    this.backgroundImage = backgroundImage;
-    this.timer = new Timer();
-    this.icon = icon;
-    this.endOfGame = new EndOfGame(game, this.icon);
-    this.isGameActive = true;
-    this.playerCard1 = new PlayerCard(
-      "Player 1",
-      // Change the image accordingly to the player1
-      assets.images.playerAnimations[0],
-      3,
-      width / 2 - 550,
-      50,
-      1
-    );
-    this.playerCard2 = new PlayerCard(
-      "Player 2",
-      // Change the image accordingly to the player2
-      assets.images.playerAnimations[12],
-      3,
-      width / 2 + 550,
-      50,
-      2
-    );
-  }
+	constructor(
+		entities: GameEntity[],
+		backgroundImage: p5.Image,
+		icon: p5.Image
+	) {
+		this.clouds = new Clouds();
+		this.flowers = new Flowers();
+		this.entities = entities;
+		this.backgroundImage = backgroundImage;
+		this.timer = new Timer();
+		this.icon = icon;
+		this.endOfGame = new EndOfGame(game, this.icon);
+		this.isGameActive = true;
+		this.playerCard1 = new PlayerCard(
+			"Player 1",
+			// Change the image accordingly to the player1
+			assets.images.playerAnimations[0],
+			3,
+			width / 2 - 550,
+			50,
+			1
+		);
+		this.playerCard2 = new PlayerCard(
+			"Player 2",
+			// Change the image accordingly to the player2
+			assets.images.playerAnimations[12],
+			3,
+			width / 2 + 550,
+			50,
+			2
+		);
+	}
 
-  public setupGameBackground() {
-    createCanvas(windowWidth, windowHeight);
-  }
+	public setupGameBackground() {
+		createCanvas(windowWidth, windowHeight);
+	}
 
-  public drawGameBackground() {
-    image(this.backgroundImage, 0, 0, width, height);
-  }
+	public drawGameBackground() {
+		image(this.backgroundImage, 0, 0, width, height);
+	}
 
-  private checkCollision() {
-    for (const entity1 of this.entities) {
-      for (const entity2 of this.entities) {
-        // Collision mellan andra spelaren och motspelares bomb
-        if (
-          entity1 instanceof Player &&
-          entity2 instanceof Bomb &&
-          entity1.getID() !== entity2.ownerId
-        ) {
-          const l1 = entity1.x;
-          const r1 = entity1.x + entity1.size;
-          const t1 = entity1.y;
-          const b1 = entity1.y + entity1.size;
+	private checkCollision() {
+		for (const entity1 of this.entities) {
+			for (const entity2 of this.entities) {
+				// Collision mellan andra spelaren och motspelares bomb
+				if (
+					entity1 instanceof Player &&
+					entity2 instanceof Bomb &&
+					entity1.getID() !== entity2.ownerId
+				) {
+					const l1 = entity1.x;
+					const r1 = entity1.x + entity1.size;
+					const t1 = entity1.y;
+					const b1 = entity1.y + entity1.size;
 
-          const l2 = entity2.x;
-          const r2 = entity2.x + entity2.size;
-          const t2 = entity2.y;
-          const b2 = entity2.y + entity2.size;
+					const l2 = entity2.x;
+					const r2 = entity2.x + entity2.size;
+					const t2 = entity2.y;
+					const b2 = entity2.y + entity2.size;
 
-          if (r1 > l2 && l1 < r2 && b1 > t2 && t1 < b2) {
-            entity1.x -= entity1.speedX;
-            entity1.y -= entity1.speedY;
-          }
-        }
-        if (entity1 instanceof Explosion && entity2 instanceof StaticObstacle) {
-          const l1 = entity1.x;
-          const r1 = entity1.x + entity1.size;
-          const l2 = entity2.x;
-          const r2 = entity2.x + entity2.size;
-          const t1 = entity1.y;
-          const b1 = entity1.y + entity1.size;
-          const t2 = entity2.y;
-          const b2 = entity2.y + entity2.size;
+					if (r1 > l2 && l1 < r2 && b1 > t2 && t1 < b2) {
+						entity1.x -= entity1.speedX;
+						entity1.y -= entity1.speedY;
+					}
+				}
+				if (entity1 instanceof Explosion && entity2 instanceof StaticObstacle) {
+					const l1 = entity1.x;
+					const r1 = entity1.x + entity1.size;
+					const l2 = entity2.x;
+					const r2 = entity2.x + entity2.size;
+					const t1 = entity1.y;
+					const b1 = entity1.y + entity1.size;
+					const t2 = entity2.y;
+					const b2 = entity2.y + entity2.size;
 
-          if (l2 < r1 && l1 < r2 && t2 < b1 && t1 < b2) {
-            entity1.shouldBeRemoved = true;
-          }
-        }
-      }
-    }
+					if (l2 < r1 && l1 < r2 && t2 < b1 && t1 < b2) {
+						entity1.shouldBeRemoved = true;
+					}
+				}
+			}
+		}
 
-    for (const entity1 of this.entities) {
-      for (const entity2 of this.entities) {
-        if (entity1 === entity2) continue;
+		for (const entity1 of this.entities) {
+			for (const entity2 of this.entities) {
+				if (entity1 === entity2) continue;
 
-        // Bomben ska inte krocka med något
-        // Explosioner ska krocka med hinder och spelare.
-        // Spelaren ska krocka med explosioner, hinder och powerups.
+				// Bomben ska inte krocka med något
+				// Explosioner ska krocka med hinder och spelare.
+				// Spelaren ska krocka med explosioner, hinder och powerups.
 
-        if (entity1 instanceof Bomb || entity2 instanceof Bomb) continue;
-        if (entity1 instanceof Player && entity2 instanceof Player) continue;
+				if (entity1 instanceof Bomb || entity2 instanceof Bomb) continue;
+				if (entity1 instanceof Player && entity2 instanceof Player) continue;
 
-        // Kolla om entitierna överlappar varandra
-        // 1. Identifiera faktiska krockar
-        // Definera höger och vänster sida för varje entitet
-        const l1 = entity1.x;
-        const r1 = entity1.x + entity1.size;
-        const l2 = entity2.x;
-        const r2 = entity2.x + entity2.size;
-        const t1 = entity1.y;
-        const b1 = entity1.y + entity1.size;
-        const t2 = entity2.y;
-        const b2 = entity2.y + entity2.size;
+				const hitBox1 = entity1.getHitBox();
+				const hitBox2 = entity2.getHitBox();
 
-        if (l2 < r1 && l1 < r2 && t2 < b1 && t1 < b2) {
-          this.reactToCollision(entity1, entity2);
-        }
-      }
-    }
-  }
+				// checks hitbox for collisions
+				if (
+					hitBox1.left < hitBox2.left + hitBox2.width &&
+					hitBox1.left + hitBox1.width > hitBox2.left &&
+					hitBox1.top < hitBox2.top + hitBox2.height &&
+					hitBox1.top + hitBox1.height > hitBox2.top
+				) {
+					this.reactToCollision(entity1, entity2);
+				}
+			}
+		}
+	}
 
-  private reactToCollision(entity1: GameEntity, entity2: GameEntity) {
-    if (
-      entity1 instanceof Explosion &&
-      entity2 instanceof RemovebleObstacle &&
-      !entity1.shouldBeRemoved
-    ) {
-      entity2.shouldBeRemoved = true;
-    }
-    if (entity1 instanceof Player && entity2 instanceof SpeedUp) {
-      entity1.increaseSpeed();
-      entity2.shouldBeRemoved = true;
-    }
-    if (entity1 instanceof Player && entity2 instanceof SlowDownOpponent) {
-      const opponent = this.getOpponent(entity1);
-      if (opponent) {
-        opponent.decreaseSpeed();
-      }
-      entity2.shouldBeRemoved = true;
-    }
-    if (entity1 instanceof Player && entity2 instanceof LongerBombRange) {
-      // fixa reaktion 
-      entity2.shouldBeRemoved = true;
-    }
-    if (entity1 instanceof Player && entity2 instanceof MoreBomb) {
-      //fixa reaktion
-      entity2.shouldBeRemoved = true;
-    }
+	private reactToCollision(entity1: GameEntity, entity2: GameEntity) {
+		if (
+			entity1 instanceof Explosion &&
+			entity2 instanceof RemovebleObstacle &&
+			!entity1.shouldBeRemoved
+		) {
+			entity2.shouldBeRemoved = true;
+		}
+		if (entity1 instanceof Player && entity2 instanceof SpeedUp) {
+			entity1.increaseSpeed();
+			entity2.shouldBeRemoved = true;
+		}
+		if (entity1 instanceof Player && entity2 instanceof SlowDownOpponent) {
+			const opponent = this.getOpponent(entity1);
+			if (opponent) {
+				opponent.decreaseSpeed();
+			}
+			entity2.shouldBeRemoved = true;
+		}
+		if (entity1 instanceof Player && entity2 instanceof LongerBombRange) {
+			// fixa reaktion
+			entity2.shouldBeRemoved = true;
+		}
+		if (entity1 instanceof Player && entity2 instanceof MoreBomb) {
+			//fixa reaktion
+			entity2.shouldBeRemoved = true;
+		}
 
+		if (
+			entity1 instanceof Player &&
+			entity2 instanceof Explosion &&
+			!entity2.shouldBeRemoved
+		) {
+			if (!entity1.isProtected) {
+				// Remove a life from the appropriate player card
+				if (entity1.getID() === this.playerCard1.playerNumber) {
+					this.playerCard1.removeLife();
+				} else if (entity1.getID() === this.playerCard2.playerNumber) {
+					this.playerCard2.removeLife();
+				}
 
-    if (
-      entity1 instanceof Player &&
-      entity2 instanceof Explosion &&
-      !entity2.shouldBeRemoved
-    ) {
-      if (!entity1.isProtected) {
-        // Remove a life from the appropriate player card
-        if (entity1.getID() === this.playerCard1.playerNumber) {
-          this.playerCard1.removeLife();
-        } else if (entity1.getID() === this.playerCard2.playerNumber) {
-          this.playerCard2.removeLife();
-        }
+				// Set the player to be protected
+				entity1.isProtected = true;
+				entity1.protectionTimer = entity1.protectionDuration;
+			}
+		}
 
-        // Set the player to be protected
-        entity1.isProtected = true;
-        entity1.protectionTimer = entity1.protectionDuration;
-      }
-    }
+		if (this.playerCard1.lives <= 0 || this.playerCard2.lives <= 0) {
+			this.handleGameOver();
+		}
 
-    if (this.playerCard1.lives <= 0 || this.playerCard2.lives <= 0) {
-      this.handleGameOver();
-    }
+		if (
+			entity1 instanceof Player &&
+			(entity2 instanceof StaticObstacle ||
+				entity2 instanceof RemovebleObstacle)
+		) {
+			entity1.x -= entity1.speedX;
+			entity1.y -= entity1.speedY;
+		}
+	}
 
-    if (
-      entity1 instanceof Player &&
-      (entity2 instanceof StaticObstacle ||
-        entity2 instanceof RemovebleObstacle)
-    ) {
-      entity1.x -= entity1.speedX;
-      entity1.y -= entity1.speedY;
-    }
-  }
+	private handleGameOver() {
+		if (this.playerCard1.lives <= 0 && this.playerCard2.lives <= 0) {
+			// Case where both players have no lives left
+			this.endOfGame.setWinner("You Both Lost!");
+		} else if (this.playerCard1.lives <= 0) {
+			// Case where player 1 has no lives left
+			this.endOfGame.setWinner("Player 2");
+			this.endOfGame.setWinnerIcon(this.playerCard2.icon);
+		} else if (this.playerCard2.lives <= 0) {
+			// Case where player 2 has no lives left
+			this.endOfGame.setWinner("Player 1");
+			this.endOfGame.setWinnerIcon(this.playerCard1.icon);
+		}
 
-  private handleGameOver() {
-    if (this.playerCard1.lives <= 0 && this.playerCard2.lives <= 0) {
-      // Case where both players have no lives left
-      this.endOfGame.setWinner("You Both Lost!");
-    } else if (this.playerCard1.lives <= 0) {
-      // Case where player 1 has no lives left
-      this.endOfGame.setWinner("Player 2");
-      this.endOfGame.setWinnerIcon(this.playerCard2.icon);
-    } else if (this.playerCard2.lives <= 0) {
-      // Case where player 2 has no lives left
-      this.endOfGame.setWinner("Player 1");
-      this.endOfGame.setWinnerIcon(this.playerCard1.icon);
-    }
+		this.endGame();
+	}
 
-    this.endGame();
-  }
+	private getOpponent(currentPlayer: Player): Player | null {
+		// Assuming there are only two players
+		return this.entities.find(
+			(entity) => entity instanceof Player && entity !== currentPlayer
+		) as Player | null;
+	}
 
-  private getOpponent(currentPlayer: Player): Player | null {
-    // Assuming there are only two players
-    return this.entities.find(
-      (entity) => entity instanceof Player && entity !== currentPlayer
-    ) as Player | null;
-  }
+	// om R1 är mindre än L2 så är det ingen krock men om L2 är mindre än R1 har vi KANSKE en krock.
+	// R2 är mindre än L1 = ingen krock, men L1 är mindre än R2 är en krock
+	// R = x + w
+	// L = x
+	// en if sats med 4 rader VILL VI HAA, 2 med x och 2 med y
 
-  // om R1 är mindre än L2 så är det ingen krock men om L2 är mindre än R1 har vi KANSKE en krock.
-  // R2 är mindre än L1 = ingen krock, men L1 är mindre än R2 är en krock
-  // R = x + w
-  // L = x
-  // en if sats med 4 rader VILL VI HAA, 2 med x och 2 med y
+	public update() {
+		if (!this.isGameActive) {
+			this.endOfGame.update();
+			return;
+		}
+		for (const entity of this.entities) {
+			entity.update(this);
+		}
+		if (this.isGameOver()) {
+			this.endGame();
+		}
+		this.checkCollision();
+		this.removeEntities();
+	}
 
-  public update() {
-    if (!this.isGameActive) {
-      this.endOfGame.update();
-      return;
-    }
-    for (const entity of this.entities) {
-      entity.update(this);
-    }
-    if (this.isGameOver()) {
-      this.endGame();
-    }
-    this.checkCollision();
-    this.removeEntities();
-  }
+	private removeEntities() {
+		for (let i = 0; i < this.entities.length; i++) {
+			if (this.entities[i].shouldBeRemoved) {
+				this.entities.splice(i, 1);
+				i--;
+			}
+		}
+		this.endOfGame.update();
+	}
 
-  private removeEntities() {
-    for (let i = 0; i < this.entities.length; i++) {
-      if (this.entities[i].shouldBeRemoved) {
-        this.entities.splice(i, 1);
-        i--;
-      }
-    }
-    this.endOfGame.update();
-  }
+	public addEntity(entity: GameEntity) {
+		this.entities.push(entity);
+	}
 
-  public addEntity(entity: GameEntity) {
-    this.entities.push(entity);
-  }
+	public startGame() {
+		this.timer.start();
+	}
 
-  public startGame() {
-    this.timer.start();
-  }
+	public endGame() {
+		this.isGameActive = false;
+		this.timer.stop();
+		this.endOfGame.show();
+	}
 
-  public endGame() {
-    this.isGameActive = false;
-    this.timer.stop();
-    this.endOfGame.show();
-  }
+	public isGameOver(): boolean {
+		// Handle the case when the time is over
+		const isTimeOver = this.timer.getTime() === "00:00";
 
-  public isGameOver(): boolean {
-    // Handle the case when the time is over
-    const isTimeOver = this.timer.getTime() === "00:00";
+		// Hndle the case when one of the players has no lives left
+		const isLifeOver =
+			this.playerCard1.lives <= 0 || this.playerCard2.lives <= 0;
 
-    // Hndle the case when one of the players has no lives left
-    const isLifeOver =
-      this.playerCard1.lives <= 0 || this.playerCard2.lives <= 0;
+		if (isTimeOver) {
+			this.endOfGame.setWinner("Time is up!");
+		} else if (isLifeOver) {
+			this.handleGameOver();
+		}
 
-    if (isTimeOver) {
-      this.endOfGame.setWinner("Time is up!");
-    } else if (isLifeOver) {
-      this.handleGameOver();
-    }
+		return isTimeOver || isLifeOver;
+	}
 
-    return isTimeOver || isLifeOver;
-  }
-
-  public draw() {
-    this.drawGameBackground();
-    for (let i = 0; i < this.entities.length; i++) {
-      this.entities[i].draw();
-    }
-    this.timer.drawTimer();
-    this.playerCard1.draw();
-    this.playerCard2.draw();
-    this.clouds.draw();
-    this.flowers.draw();
-    this.endOfGame.draw();
-  }
+	public draw() {
+		this.drawGameBackground();
+		for (let i = 0; i < this.entities.length; i++) {
+			this.entities[i].draw();
+		}
+		this.timer.drawTimer();
+		this.playerCard1.draw();
+		this.playerCard2.draw();
+		this.clouds.draw();
+		this.flowers.draw();
+		this.endOfGame.draw();
+	}
 }
